72454a1b56e41469299190d84f1b4c5c4d9d5870
Shivesh Navin <shiveshnavin@gmail.com>
2023-04-06 01:00:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,45 @@ const ffmpegPath = require('ffmpeg-static');
 // Set the path to the FFmpeg binary within the Node.js application
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-app.get('/stream', (req, res) => {
+
+async function getSupportedFormat() {
+  const xcbgrabSupported = await checkInputFormat('xcbgrab');
+  if (xcbgrabSupported) {
+    return 'xcbgrab';
+  }
+
+  const x11grabSupported = await checkInputFormat('x11grab');
+  if (x11grabSupported) {
+    return 'x11grab';
+  }
+
+  return null;
+}
+
+function checkInputFormat(format) {
+  return new Promise((resolve, reject) => {
+    const command = ffmpeg();
+    command.input(':0.0')
+      .inputFormat(format)
+      .on('error', (err) => {
+        if (err.message.includes('input format not recognized')) {
+          resolve(false);
+        } else {
+          reject(err);
+        }
+      })
+      .on('end', () => {
+        resolve(true);
+      })
+      .output('/dev/null')
+      .run();
+  });
+}
+
+app.get('/stream',async (req, res) => {
     const command = ffmpeg()
         .input(':10.0')
-        .inputFormat('xcbgrab')
+        .inputFormat(await getSupportedFormat())
         .videoCodec('libx264')
         .inputFPS(25)
         .size('1024x768')
